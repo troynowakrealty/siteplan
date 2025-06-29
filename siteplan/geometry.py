d22ca5e5747015f71d5bc85927559a2ede03e859
Troy Nowak <troynowak@troys-air.mynetworksettings.com>
2025-06-29 16:31:35 -0400

    fix: update Rectangle ctor to support both signatures and add properties

--- siteplan/geometry.py
+++ siteplan/geometry.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from math import sqrt
+from typing import Union, Tuple
 
 @dataclass(frozen=True)
 class Point:
@@ -11,22 +12,49 @@ class Size:
     width: float = 0
     height: float = 0
 
-@dataclass(frozen=True)
 class Rectangle:
-    origin: Point
-    size: Size
+    def __init__(self, *args):
+        # Accept (x, y, width, height)
+        if len(args) == 4 and all(isinstance(v, (int, float)) for v in args):
+            x, y, w, h = args
+            self.origin = Point(x, y)
+            self.size = Size(w, h)
+        # Accept (Point, Size)
+        elif len(args) == 2 and isinstance(args[0], Point) and isinstance(args[1], Size):
+            self.origin = args[0]
+            self.size = args[1]
+        else:
+            raise TypeError("Rectangle requires (x, y, w, h) or (Point, Size)")
+
+    @property
+    def x(self):
+        return self.origin.x
+
+    @property
+    def y(self):
+        return self.origin.y
+
+    @property
+    def width(self):
+        return self.size.width
+
+    @property
+    def height(self):
+        return self.size.height
 
     def center(self) -> Point:
         return Point(
-            self.origin.x + self.size.width / 2,
-            self.origin.y + self.size.height / 2
+            self.x + self.width/2,
+            self.y + self.height/2
         )
 
     def move(self, dx: float, dy: float) -> 'Rectangle':
         return Rectangle(
-            origin=Point(self.origin.x + dx, self.origin.y + dy),
-            size=self.size
+            self.x + dx,
+            self.y + dy,
+            self.width,
+            self.height
         )
 
-def distance(a: tuple, b: tuple) -> float:
-    return sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+def distance(a: Tuple[float, float], b: Tuple[float, float]) -> float:
+    return sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2)
