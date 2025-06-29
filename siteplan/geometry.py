9f8647db53d49b4af9ba3b1d0b2ba245793efdf9
Troy Nowak <troynowak@troys-air.mynetworksettings.com>
2025-06-29 16:30:57 -0400

    fix: rewrite geometry.py to valid implementation

--- siteplan/geometry.py
+++ siteplan/geometry.py
@@ -1,18 +1,25 @@
 from dataclasses import dataclass
+from math import sqrt
 
-
-@dataclass
-class Rectangle:
-    pass
-
-
-from math import sqrtnndef distance(a: tuple, b: tuple) -> float:n    return sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
 @dataclass
 class Point:
-    x: int = 0
-    y: int = 0
+    x: float = 0
+    y: float = 0
 
 @dataclass
 class Size:
-    width: int = 0
-    height: int = 0
+    width: float = 0
+    height: float = 0
+
+@dataclass
+class Rectangle:
+    x: float
+    y: float
+    width: float
+    height: float
+
+    def center(self):
+        return Point(self.x + self.width / 2, self.y + self.height / 2)
+
+def distance(a: tuple, b: tuple) -> float:
+    return sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
